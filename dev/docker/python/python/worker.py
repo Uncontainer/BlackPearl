e01635bef5db10464edda8c92bdc84ae71ab9a6f
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2016-09-24 17:36:34 +0300

    working rqscheduler, worker, postgre

--- dev/docker/python/python/worker.py
+++ dev/docker/python/python/worker.py
@@ -1,6 +1,7 @@
 from redis import StrictRedis
 from rq import Worker, Queue, Connection
-import requests
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+import requests, sqlalchemy
 
 listen = ['default']
 
@@ -8,10 +9,19 @@ redis_url = 'redis://127.0.0.1:6379'
 
 conn = StrictRedis.from_url(redis_url)
 
+postgres_url = 'postgresql://worker:redcarpet@localhost:5432/workers'
+
+postgres_con = sqlalchemy.create_engine(postgres_url, client_encoding='utf8')
+
+meta = sqlalchemy.MetaData(bind=postgres_con, reflect=True)
+
 def count_words_at_url(url):
     url = 'http://'+url
     resp = requests.get(url)
-    return len(resp.text.split())
+    result = len(resp.text.split())
+    clause = meta.tables['jobs'].insert().values(results=result)
+    entry = postgres_con.execute(clause)
+    print result, entry.inserted_primary_key
 
 if __name__ == "__main__":
   with Connection(conn):
