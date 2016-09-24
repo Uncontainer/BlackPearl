e5b04288f6e39dd1d850a5bfa8d6b2787d43b17f
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2016-09-24 12:43:39 +0300

    using name flask in nginx conf

--- dev/docker/python/python/app.py
+++ dev/docker/python/python/app.py
@@ -5,15 +5,24 @@ import glob, requests
 
 app = Flask(__name__)
 
+r = redis.StrictRedis(host='redis', port=6379, db=0)
+
+q = Queue(connection=r)
+
 @app.route("/")
 def hello():
-    test = ''
-    outside_root = glob.glob('/*')
-    for i in outside_root:
-      test = test+i
-    return "<h1 style='color:blue'>Hello There!</h1><br>"+test+"<br>Sanity test over here"
+    return "<h1 style='color:blue'>Hello There!</h1><br>"
 
+@app.route("/queue/<url>")
+def queue(url):
+    job = q.enqueue(
+             count_words_at_url, url)
+    print job.result
+    return job
 
+def count_words_at_url(url):
+    resp = requests.get(url)
+    return len(resp.text.split())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
