a3ae3a512c544c0be8330ea4cdbd638c1f15a58f
sandeep srinivasa <sss@lambdacurry.com>
2016-09-29 16:14:01 +0530

    fixed python build issues

--- python/build.sh
+++ python/build.sh
@@ -1,8 +1,8 @@
 cd /opt/python/sources && \
-wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz && \
-tar zxvof Python-2.7.10.tgz && \
+wget https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz && \
+tar zxvof Python-2.7.12.tgz && \
 
-cd Python-2.7.10 && \
+cd Python-2.7.12 && \
 
 ./configure --prefix=/opt/python --enable-shared LDFLAGS=-Wl,-rpath=/opt/python/lib && \
 
