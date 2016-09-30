7081b3330296b1df1aeacd7ce13d207d91b05535
sandeep srinivasa <sss@lambdacurry.com>
2016-09-30 16:31:02 +0530

    fixed pgbouncer with the correct config

---
+++ pgbouncer/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+PG_CONFIG=/etc/pgbouncer/pgbouncer.ini
+PG_USER=postgres
+PG_LOG=/var/log/pgbouncer
+
+
+echo "Starting pgbouncer..."
+exec pgbouncer -u pgbouncer  $PG_CONFIG
+
+
+
